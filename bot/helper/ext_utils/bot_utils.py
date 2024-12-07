4e9797701ffc8ed0062c91c6451395497918f06a
ajaykumarsb170 <156455377+ajaykumarsb170@users.noreply.github.com>
2024-12-07 13:32:41 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -234,8 +234,8 @@ def progress_bar(pct):
         pct = float(pct.strip("%"))
     p = min(max(pct, 0), 100)
     c_full = int((p + 5) // 10)
-    p_str = "🟩" * c_full
-    p_str += "⬜" * (10 - c_full)
+    p_str = "🪢" * c_full
+    p_str += "□" * (10 - c_full)
     return p_str
 
 
