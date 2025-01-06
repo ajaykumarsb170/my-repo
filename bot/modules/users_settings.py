4f050fb6080a654925d10705a74ba40bf1fe9669
ajaykumarsb170 <156455377+ajaykumarsb170@users.noreply.github.com>
2025-01-06 10:40:55 +0530

    Update users_settings.py

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -259,7 +259,7 @@ async def update_user_settings(
     user_id = query.from_user.id
     thumbnail = f"Thumbnails/{user_id}.jpg"
     if not ospath.exists(thumbnail):
-        thumbnail = "https://graph.org/file/73ae908d18c6b38038071.jpg"
+        thumbnail = "https://images8.alphacoders.com/125/1250406.jpg"
     await edit_message(query.message, msg, button, thumbnail)
 
 
@@ -269,7 +269,7 @@ async def user_settings(_, message):
     user_id = message.from_user.id
     thumbnail = f"Thumbnails/{user_id}.jpg"
     if not ospath.exists(thumbnail):
-        thumbnail = "https://graph.org/file/73ae908d18c6b38038071.jpg"
+        thumbnail = "https://images8.alphacoders.com/125/1250406.jpg"
     x = await send_message(message, msg, button, thumbnail)
     await five_minute_del(message)
     await delete_message(x)
