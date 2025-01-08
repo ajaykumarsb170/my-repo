37575d52542a92f349497462b6dfd63ce7adbcea
ajaykumarsb170 <156455377+ajaykumarsb170@users.noreply.github.com>
2025-01-08 20:31:53 +0530

    Update tasks_listener.py

--- bot/helper/listeners/tasks_listener.py
+++ bot/helper/listeners/tasks_listener.py
@@ -507,7 +507,7 @@ class MirrorLeechListener:
                 if self.isSuperGroup:
                     await send_message(
                         self.message,
-                        f"{msg}<blockqoute><b>Files has been sent to your DM</b><blockqoute>",
+                        f"{msg}<blockqoute><b>Files has been sent to your DM</b>\n",
                         inboxButton.column(1),
                     )
                 else:
@@ -553,7 +553,7 @@ class MirrorLeechListener:
             if self.isSuperGroup:
                 await send_message(
                     self.message,
-                    f"{msg} <blockqoute><b>Links has been sent to your DM</b><blockqoute>",
+                    f"{msg} <blockqoute><b>Links has been sent to your DM</b>\n",
                     inboxButton.column(1),
                 )
             else:
